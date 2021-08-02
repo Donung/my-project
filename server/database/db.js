31fe90f9b4068371fe89a5f41be3056e7e4adb0a
Donung <58160282@go.buu.ac.th>
2021-08-02 22:15:43 +0700

    first edit

--- server/database/db.js
+++ server/database/db.js
@@ -1,3 +1,5 @@
 module.exports = {
     db: 'mongodb+srv://dbUser:dbUserPassword@cluster0.okr30.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
+
+/*db: 'mongodb://localhost:27017/reactdb'*/
